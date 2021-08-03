a1c1f9a996888ecb8d3b09b5853a836eaa507d4e
aleksei24 <lexa24032@gmail.com>
2021-08-03 12:11:47 +0300

    mongoose has been required and set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 require('dotenv').config();
 
@@ -7,6 +8,21 @@ app.get('/', (req, res) => {
     res.send('The project has been launched');
 });
 
+const connect_str = process.env.CONNECTION;
+
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 });
+
+mongoose
+    .connect(connect_str, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('MongoDB connection is running');
+    })
+    .catch((err) => {
+        console.error('MongoDB connection failed: ', err.message);
+    });
