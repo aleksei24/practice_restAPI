464101f8121b1013f2d8dd0c81761387f8dfdd9a
aleksei24 <lexa24032@gmail.com>
2021-08-03 16:18:30 +0300

    rules for app.js have been set

---
+++ models/app.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const appSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 150,
+    },
+    author: String,
+    uid: String,
+    isComplete: Boolean,
+    date: { type: Date, default: new Date() },
+});
+
+const appTask = mongoose.model('Task', appSchema);
+
+exports.appTask = appTask;
