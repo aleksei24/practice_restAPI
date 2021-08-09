45d5ed54e48b1f36fe3c8f49dd851597edf693fc
aleksei24 <lexa24032@gmail.com>
2021-08-09 11:43:39 +0300

    parameters for post method of router

--- routes/router.js
+++ routes/router.js
@@ -1,9 +1,24 @@
 const { appTask } = require('../models/app');
 const express = require('express');
+const joi = require('joi');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+    const schema = joi.object({
+        name: joi.string().min(3).max(150).required(),
+        author: joi.string().min(3).max(30),
+        uid: joi.string(),
+        isComplete: joi.boolean(),
+        date: joi.date(),
+    });
+
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
     const { name, author, isComplete, date, uid } = req.body;
 
     let routerTask = new appTask({
